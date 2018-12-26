3d78c7107ade8b18f98a026de84d132804950aba
Maison Galvao <maisongalvao144@gmail.com>
2018-12-26 17:07:06 -0400

    Aula04 - Enviando dados com Ajax

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,5 +1,7 @@
 $('#trocaFrase').click(fraseAleatoria);
 
+$('#botaoFraseId').click(buscaFrasePorId);
+
 function fraseAleatoria() {
     $('#spinner').show();
     $.get('http://localhost:3000/frases',trocaFraseAleatoria)
@@ -15,10 +17,37 @@ function fraseAleatoria() {
 
 }
 
+
+
 function trocaFraseAleatoria(data)  {
     var frase = $('.frase');
     var numeroAleatorio = Math.floor(Math.random()*data.length);
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo)
+}
+
+function buscaFrasePorId(){
+    var idEscolhido = $('#idFrase').val();
+    $('#spinner').show();
+    var dados = {id:idEscolhido}
+    console.log(dados.id)
+    $.get('http://localhost:3000/frases',dados,trocaFrasePeloId)
+        .fail(function () {
+            $('#erro').show();
+            setTimeout(function () {
+                $('#erro').toggle()
+            },1900);
+        })
+        .always(function () {
+            $('#spinner').toggle();
+        })
+}
+
+function trocaFrasePeloId(data) {
+    console.log(data.texto)
+     var frase = $('.frase');
+     frase.text(data.texto);
+     atualizaTamanhoFrase();
+     atualizaTempoInicial(data.tempo)
 }
