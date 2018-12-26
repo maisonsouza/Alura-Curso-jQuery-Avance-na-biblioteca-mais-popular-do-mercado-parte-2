c24eeaa4efab9aeb9c32ed381d2230b3a496800d
Maison Galvao <maisongalvao144@gmail.com>
2018-12-26 14:47:09 -0400

    Tratando erros e melhorando a experiencia

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,11 +1,24 @@
 $('#trocaFrase').click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get('http://localhost:3000/frases',function (data) {
+    $('#spinner').show();
+    $.get('http://localhost:3000/frases',trocaFraseAleatoria)
+        .fail(function () {
+            $('#erro').show();
+            setTimeout(function () {
+                $('#erro').toggle()
+            },1900);
+        })
+    .always(function () {
+        $('#spinner').toggle();
+    })
+
+}
+
+function trocaFraseAleatoria(data)  {
     var frase = $('.frase');
     var numeroAleatorio = Math.floor(Math.random()*data.length);
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo)
-    })
 }
